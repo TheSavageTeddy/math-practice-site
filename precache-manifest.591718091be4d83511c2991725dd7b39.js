83078f38cc6688782df9df80eb71f4f00d0d549f
TheSavageTeddy <51810476+TheSavageTeddy@users.noreply.github.com>
2022-09-14 20:21:16 +0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.591718091be4d83511c2991725dd7b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e277f101cb582bb15ec4",
+    "url": "/math-practice-website/static/css/main.2b394af3.chunk.css"
+  },
+  {
+    "revision": "e277f101cb582bb15ec4",
+    "url": "/math-practice-website/static/js/main.3d5ead20.chunk.js"
+  },
+  {
+    "revision": "29d4a3aea1ec014971d1",
+    "url": "/math-practice-website/static/js/runtime~main.ca00fd51.js"
+  },
+  {
+    "revision": "f89988676b5548283aaf",
+    "url": "/math-practice-website/static/js/2.f1c12e24.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/math-practice-website/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "38eaf2b18e3d6dbcd24ce07cef14734e",
+    "url": "/math-practice-website/index.html"
+  }
+];
